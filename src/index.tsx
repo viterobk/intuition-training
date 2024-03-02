d1e1cc768f5bfda4acb984a16221668e03da4e08
Viktor Shchelkunov <VShchelkunov@detmir.ru>
2024-03-02 22:00:01 +0300

    Remove redundant code

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserHistory } from 'history';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import theme from './theme';
-
-const history = createBrowserHistory();
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
