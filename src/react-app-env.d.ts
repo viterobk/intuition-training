0d2f1fde6dcd63aa14a95468741f2f69f8deb734
Viktor Shchelkunov <VShchelkunov@detmir.ru>
2026-09-12 11:45:23 +0300

    Fix

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
